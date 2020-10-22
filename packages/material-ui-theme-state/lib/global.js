b841d7b64bf8a70da55c07956b30fd485933736e
bluelovers <codelovers@users.sourceforge.net>
2020-10-22 12:55:25 +0800

    build: build files

--- packages/material-ui-theme-state/lib/global.js
+++ packages/material-ui-theme-state/lib/global.js
@@ -1,10 +1,12 @@
 "use strict";
 /// <reference types="react" />
 /// <reference types="next" />
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createTheme = exports.store = exports.useTheme = void 0;
-const tslib_1 = require("tslib");
-const index_1 = tslib_1.__importDefault(require("./index"));
+const index_1 = __importDefault(require("./index"));
 const globalThemeState = index_1.default();
 exports.useTheme = globalThemeState.useTheme, exports.store = globalThemeState.store, exports.createTheme = globalThemeState.createTheme;
 exports.default = globalThemeState;
