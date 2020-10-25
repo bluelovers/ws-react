911a4ba693de383302dce310f4ada86fa6c644ab
bluelovers <codelovers@users.sourceforge.net>
2020-10-25 08:07:05 +0800

    .

--- packages/@lazy-react/react-suspense/index.js
+++ packages/@lazy-react/react-suspense/index.js
@@ -4,12 +4,11 @@ exports.ReactSuspense = void 0;
 const tslib_1 = require("tslib");
 const react_1 = tslib_1.__importStar(require("react"));
 // @ts-ignore
-const ReactSuspense = ({ fallback, ...prop }) => {
+exports.ReactSuspense = ({ fallback, ...prop }) => {
     if (typeof window === 'undefined') {
         return fallback;
     }
     return react_1.default.createElement(react_1.Suspense, Object.assign({}, prop, { fallback: fallback }));
 };
-exports.ReactSuspense = ReactSuspense;
 exports.default = exports.ReactSuspense;
 //# sourceMappingURL=index.js.map
