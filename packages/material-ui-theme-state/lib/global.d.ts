93f6283a33b573ed3d756a1ce1381a505d4f0846
bluelovers <codelovers@users.sourceforge.net>
2020-10-27 06:12:12 +0800

    build: build files

--- packages/material-ui-theme-state/lib/global.d.ts
+++ packages/material-ui-theme-state/lib/global.d.ts
@@ -1,14 +1,14 @@
 /// <reference types="react" />
 import { IThemeExtra } from './index';
 declare const globalThemeState: {
-    store: WeakMap<import("@material-ui/core").Theme, (theme: import("react").SetStateAction<Required<import("@material-ui/core").Theme> | Required<import("@material-ui/core").ThemeOptions>>, autoSpread?: boolean) => import("ts-type").ITSResolvable<void | import("@material-ui/core").ThemeOptions | import("@material-ui/core").Theme>>;
+    store: WeakMap<import("@material-ui/core").Theme, (theme: import("react").SetStateAction<Required<import("@material-ui/core").Theme> | Required<import("@material-ui/core").ThemeOptions>>, autoSpread?: boolean) => import("ts-type").ITSResolvable<void | import("@material-ui/core").Theme | import("@material-ui/core").ThemeOptions>>;
     useTheme(): Required<IThemeExtra<import("@material-ui/core").Theme, import("@material-ui/core").ThemeOptions>>;
     createTheme(options?: import("@material-ui/core").ThemeOptions, optsExtra?: {
-        setTheme?: import("react").Dispatch<import("react").SetStateAction<import("@material-ui/core").ThemeOptions | import("@material-ui/core").Theme>>;
+        setTheme?: import("react").Dispatch<import("react").SetStateAction<import("@material-ui/core").Theme | import("@material-ui/core").ThemeOptions>>;
     }, ...args: object[]): IThemeExtra<import("@material-ui/core").Theme, import("@material-ui/core").ThemeOptions>;
 };
-export declare const useTheme: () => Required<IThemeExtra<import("@material-ui/core").Theme, import("@material-ui/core").ThemeOptions>>, store: WeakMap<import("@material-ui/core").Theme, (theme: import("react").SetStateAction<Required<import("@material-ui/core").Theme> | Required<import("@material-ui/core").ThemeOptions>>, autoSpread?: boolean) => import("ts-type").ITSResolvable<void | import("@material-ui/core").ThemeOptions | import("@material-ui/core").Theme>>, createTheme: (options?: import("@material-ui/core").ThemeOptions, optsExtra?: {
-    setTheme?: import("react").Dispatch<import("react").SetStateAction<import("@material-ui/core").ThemeOptions | import("@material-ui/core").Theme>>;
+export declare const useTheme: () => Required<IThemeExtra<import("@material-ui/core").Theme, import("@material-ui/core").ThemeOptions>>, store: WeakMap<import("@material-ui/core").Theme, (theme: import("react").SetStateAction<Required<import("@material-ui/core").Theme> | Required<import("@material-ui/core").ThemeOptions>>, autoSpread?: boolean) => import("ts-type").ITSResolvable<void | import("@material-ui/core").Theme | import("@material-ui/core").ThemeOptions>>, createTheme: (options?: import("@material-ui/core").ThemeOptions, optsExtra?: {
+    setTheme?: import("react").Dispatch<import("react").SetStateAction<import("@material-ui/core").Theme | import("@material-ui/core").ThemeOptions>>;
 }, ...args: object[]) => IThemeExtra<import("@material-ui/core").Theme, import("@material-ui/core").ThemeOptions>;
 export { IThemeExtra };
 export default globalThemeState;
