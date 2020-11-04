d538ca01fcb905eb8d11b03f44da59d9340ba2a3
bluelovers <codelovers@users.sourceforge.net>
2020-11-05 04:41:13 +0800

    build: build files

--- packages/material-ui-theme-state/esm/index.js
+++ packages/material-ui-theme-state/esm/index.js
@@ -3,12 +3,34 @@
  */
 import _createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import _useTheme from '@material-ui/core/styles/useTheme';
+import { useState } from 'react';
 export function create(options = {}) {
     let { store } = options;
     store = store || new WeakMap();
     const useTheme = function useTheme() {
         const theme = _useTheme();
         let setTheme = store.get(theme);
+        if (typeof setTheme === 'undefined') {
+            const [theme2, _setTheme] = useState(theme);
+            setTheme = async function (value, autoSpread) {
+                if (autoSpread && typeof value === 'function') {
+                    // @ts-ignore
+                    return _setTheme(old => {
+                        // @ts-ignore
+                        let n = value(old);
+                        if (n && typeof n === 'object') {
+                            return {
+                                ...n
+                            };
+                        }
+                        return n;
+                    });
+                }
+                // @ts-ignore
+                return _setTheme(value);
+            };
+            store.set(theme, setTheme);
+        }
         return {
             theme,
             setTheme,
